9dc254afaa6d26a2657f6cd210dc0b8c13335ebb
rahul.agrawal <rahul.agrawal@glu.lu>
2018-09-03 17:54:53 +0530

    test commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 git checkout master
-touch terraform.tfstate
-git add terraform.tfstate
+touch terraform.tfstat
+git add terraform.tfstat
 git commit -m "adding terraform state"
 git push origin master
